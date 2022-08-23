a1d31e5c613f53d0a8f4704a701e1fe631a46cbb
taoisu <csgxy123@hotmail.com>
2022-08-23 05:16:46 +0000

    barrier

--- ldm/models/diffusion/ddim.py
+++ ldm/models/diffusion/ddim.py
@@ -184,7 +184,9 @@ class DDIMSampler(object):
         total_steps = timesteps if ddim_use_original_steps else timesteps.shape[0]
         print(f"Running DDIM Sampling with {total_steps} timesteps")
 
-        iterator = tqdm(time_range, desc='DDIM Sampler', total=total_steps)
+        rank = torch.distributed.get_rank()
+        disable = rank not in (-1, 0)
+        iterator = tqdm(time_range, desc='DDIM Sampler', total=total_steps, disable=disable)
 
         for i, step in enumerate(iterator):
             index = total_steps - i - 1
