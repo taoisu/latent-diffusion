7848217dab26fa96a3df5587fe6e7973f3431f63
taoisu <csgxy123@hotmail.com>
2022-09-17 02:49:33 +0000

    cloud train txtimg inpaint

---
+++ cloud/entry_avid_train_inpaint_128-vitt10.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -xe
+
+printenv;
+
+[ "$AZUREML_DATAREFERENCE_ocrd" ]
+
+pushd /tmp; python3 -m pip uninstall latent-diffusion -y; popd;
+python3 -m pip install -e .;
+
+export AVID_ROOT_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/data/avidxchange"
+ls "$AVID_ROOT_DIR";
+
+export LOG_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/logs/inpaintavid128"
+export TOKENIZERS_PARALLELISM="false"
+export AVID_FONT_PATH="$(pwd)/assets/FreeSerif.ttf"
+
+python3 main.py                                                                 \
+    --scale_lr false                                                            \
+    --base configs/just-diffusion/txtinpaintavid128_fsdp-fairscale-vitt10.yaml  \
+    --accelerator gpu                                                           \
+    --devices 0,1,2,3,4,5,6,7                                                   \
+    -t                                                                          \
+    -l "$LOG_DIR"
