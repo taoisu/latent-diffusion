893e4cf64039b98ac2c4175609864015637008d7
Xiangyu Guo <csgxy123@hotmail.com>
2022-09-17 17:25:50 -0700

    FrozenPretrainedImageEmbedder (#38)

--- ldm/modules/encoders/modules.py
+++ ldm/modules/encoders/modules.py
@@ -9,10 +9,11 @@ import torch.nn as nn
 from einops import rearrange
 from functools import partial
 from torch import Tensor
-from typing import Dict, List
+from typing import Dict, List, OrderedDict
 from einops import rearrange, repeat
 
 from transformers import AutoTokenizer
+from transformers import ViTModel, BeitModel, CLIPVisionModel, Swinv2Model
 from transformers.models.t5.modeling_t5 import T5Block, T5EncoderModel
 
 from PIL import Image
@@ -291,9 +292,99 @@ class TextImageInpaintEmbedder(TextImageEmbedder):
         return ret
 
 
+class FrozenPretrainedImageEmbedder(AbstractEncoder):
+    '''
+    Use pretrained huggingface transformer vision encoder
+
+    Support the following models:
+    * CLIPVisionModel
+    * ViTModel (google/vit-*)
+    * Swinv2Model
+    * BeitModel
+    '''
+    def __init__(
+        self,
+        model_name:str,
+        image_size:int=None,
+        canvas_pad:int=12,
+        with_concat:bool=True,
+        with_emb:bool=True,
+        with_crossattn:bool=True,
+        with_mask:bool=True,
+    ):
+        super().__init__()
+        if model_name.startswith('google/vit-'):
+            model_class = ViTModel
+        elif model_name.startswith('openai/clip-'):
+            model_class = CLIPVisionModel
+        elif model_name.startswith('microsoft/swinv2-'):
+            model_class = Swinv2Model
+        elif model_name.startswith('microsoft/beit-'):
+            model_class = BeitModel
+        else:
+            raise ValueError()
+        self.model = model_class.from_pretrained(model_name)
+        self.canvas_pad = canvas_pad
+        self.with_concat = with_concat
+        self.with_emb = with_emb
+        self.with_crossattn = with_crossattn
+        self.with_mask = with_mask
+        self.image_size = image_size
+
+    def prep_text_cond(self, cond:Dict):
+        txt_images = cond['txt_image']
+        imgs_tsr, cc_imgs_tsr = [], []
+        cc_img_size, canvas_pad = self.image_size, self.canvas_pad
+        image_size = self.model.config.image_size
+        hf_img_size = image_size // 2
+        canvas_size = image_size - canvas_pad * 2
+        for txt_image in txt_images:
+            w, h = txt_image.size
+            l = max(w, h)
+            scale = canvas_size / l
+            if scale < 1.:
+                w, h = int(w * scale), int(h * scale)
+                txt_image = txt_image.resize((w, h), Image.Resampling.BICUBIC)
+            cond_image = Image.new('RGB', (image_size, image_size), 'white')
+            cond_image.paste(txt_image, (hf_img_size - w // 2, hf_img_size - h // 2))
+            cond_image_tsr = np.array(cond_image).astype(np.uint8)
+            imgs_tsr.append(cond_image_tsr)
+            cc_cond_image = cond_image.resize((cc_img_size, cc_img_size), Image.Resampling.BICUBIC)
+            cc_cond_image = cc_cond_image.convert('L')
+            cc_cond_image_tsr = np.array(cc_cond_image)[..., np.newaxis].astype(np.uint8)
+            cc_imgs_tsr.append(cc_cond_image_tsr)
+        imgs_tsr = np.stack(imgs_tsr)
+        imgs_tsr = (imgs_tsr/127.5-1.0).astype(np.float32)
+        imgs_tsr = torch.from_numpy(imgs_tsr).to(cond['image'])
+        imgs_tsr = rearrange(imgs_tsr, 'b h w c -> b c h w')
+        cc_imgs_tsr = np.stack(cc_imgs_tsr)
+        cc_imgs_tsr = (cc_imgs_tsr/127.5-1.0).astype(np.float32)
+        cc_imgs_tsr = torch.from_numpy(cc_imgs_tsr).to(cond['image'])
+        cc_imgs_tsr = rearrange(cc_imgs_tsr, 'b h w c -> b c h w')
+        return imgs_tsr, cc_imgs_tsr
+
+    def forward(self, **kwargs):
+        return self.model(**kwargs)
+
+    def encode(self, cond:Dict):
+        imgs_tsr, cc_imgs_tsr = self.prep_text_cond(cond)
+        outputs = self.model(imgs_tsr)
+        ret = OrderedDict()
+        if self.with_concat:
+            ret.update({ 'c_concat': cc_imgs_tsr })
+        if self.with_emb:
+            ret.update({ 'c_emb': outputs.pooler_output })
+        if self.with_crossattn:
+            ret.update({ 'c_crossattn': outputs.last_hidden_state })
+        if self.with_mask:
+            ret.update({ 'c_mask': rearrange(cond['mask'], 'b h w c -> b c h w') })
+        ret.update({ 'c_name': 'txtimg' })
+        return ret
+
+
 class FrozenPretrainedTextEmbedder(AbstractEncoder):
     '''
-    Use pretrained huggingface transformer text encoder
+    Use pretrained huggingface transformer language encoder
     '''
     def __init__(
         self,
