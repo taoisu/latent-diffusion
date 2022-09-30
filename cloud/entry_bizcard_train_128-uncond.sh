c0f3ff9aa88e0a268ef9cebccd8a5b52a0641831
David Tang <weitan@microsoft.com>
2022-09-30 11:18:45 +0800

    - Refine namings   - bizcard512_fsdp-fairscale > bizcard_sr_512x4-fairscale   - bizcard128_fsdp-fairscale-t5xl.yaml > bizcard_128-fairscale-uncond   - entry_bizcard_train_inpaint_512 > entry_bizcard_train_sr_512x4   - entry_bizcard_train_inpaint_128-t5xxl > entry_bizcard_train_128-uncond

--- cloud/entry_bizcard_train_128-uncond.sh
+++ cloud/entry_bizcard_train_128-uncond.sh
@@ -12,13 +12,14 @@ python3 -m pip install -e .;
 export BIZCARD_ROOT_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/data/business_card"
 ls "$BIZCARD_ROOT_DIR";
 
-export LOG_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/logs/business_card/super_res"
+export LOG_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/logs/business_card"
 export TOKENIZERS_PARALLELISM="false"
 
 python3 main.py                                                                 \
     --scale_lr false                                                            \
-    --base configs/just-diffusion/bizcard512_fsdp-fairscale.yaml      \
+    --base configs/just-diffusion/bizcard_128-fairscale-uncond.yaml \
     --accelerator gpu                                                           \
     --devices 0,1,2,3,4,5,6,7                                                   \
     -t                                                                          \
     -l "$LOG_DIR"
+
--- cloud/entry_bizcard_train_sr_512x4.sh
+++ cloud/entry_bizcard_train_sr_512x4.sh
@@ -12,14 +12,13 @@ python3 -m pip install -e .;
 export BIZCARD_ROOT_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/data/business_card"
 ls "$BIZCARD_ROOT_DIR";
 
-export LOG_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/logs/business_card"
+export LOG_DIR="${AZUREML_DATAREFERENCE_ocrd}/Vertical/vdipainter/logs/business_card/super_res"
 export TOKENIZERS_PARALLELISM="false"
 
 python3 main.py                                                                 \
     --scale_lr false                                                            \
-    --base configs/just-diffusion/bizcard128_fsdp-fairscale-t5xl.yaml \
+    --base configs/just-diffusion/bizcard_sr_512x4-fairscale.yaml      \
     --accelerator gpu                                                           \
     --devices 0,1,2,3,4,5,6,7                                                   \
     -t                                                                          \
     -l "$LOG_DIR"
-    # -r "$LOG_DIR/2022-09-20T04-14-37_txtinpaintbizcard128_fsdp-fairscale-t5xl/checkpoints/last.ckpt"
